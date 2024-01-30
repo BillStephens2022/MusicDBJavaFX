9f59f3d428b73f1802b26ae35b7f76dfe1278590
BillStephens2022 <two4onebill@yahoo.com>
2024-01-30 09:51:17 -0500

    copied in models folder from other MusicDBJava project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.billstephens.musicdbjavafx {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.billstephens.musicdbjavafx to javafx.fxml;
     exports com.billstephens.musicdbjavafx;
